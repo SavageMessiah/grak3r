55d09d0ef7aa21e80307cec4514ad6adcd2bdcf6
Ken Allen <kallen@lgscout.com>
2016-10-27 19:00:17 -0400

    tests!

---
+++ test/grak3r/graker_test.clj
@@ -0,0 +1,26 @@
+(ns grak3r.graker-test
+  (:require[clojure.test :refer :all]
+           [grak3r.graker :as g]))
+
+(def test-graker {:modules [(reify g/Module
+                              (handle-grake [_ _ _] "test-module"))]
+                  :builtins {'test (fn [_ _] "test-builtin")}
+                  :seed 1
+                  :rand (java.util.Random. 1)})
+
+(deftest test-basics
+  (are [g e] (= e (g/grake g test-graker))
+    "test" "test"
+    "" ""
+    #{"test"} "test"
+    #{} ""
+    #{"test1" "test2"} "test1"
+    '(test "blah") "test-builtin"
+    :test "test-module"
+    {} "test-module"
+    ["cats" "horses"] "cats horses"
+    [] ""
+    ["cats"] "cats"
+    ["cats" "," "horses"] "cats, horses"
+    ["cats" "" "horses"] "cats horses"))
+
